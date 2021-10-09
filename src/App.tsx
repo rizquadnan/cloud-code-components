b3ba807ffbc4efbd50273455c7d736e1b22d53b7
adnan <rizquadnan@gmail.com>
2021-10-09 17:12:29 +0700

    extend chakra theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import {
   VStack,
   Code,
   Grid,
-  theme,
 } from "@chakra-ui/react"
+import theme from './theme';
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 
 export const App = () => (
