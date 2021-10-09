ce33a0836fb3eb23545dc8ec98f7e34517387b96
adnan <rizquadnan@gmail.com>
2021-10-09 17:09:34 +0700

    addstorybook

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../src/theme'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+const withChakra = (StoryFn) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <StoryFn />
+    </ChakraProvider>
+  )
+}
+
+export const decorators = [withChakra]
