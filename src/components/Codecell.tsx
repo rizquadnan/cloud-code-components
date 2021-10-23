7d395bfa8fc205fa05911e521ad18cb5b92b5fd0
adnan <rizquadnan@gmail.com>
2021-10-23 13:00:16 +0700

    done number and loading

--- src/components/Codecell.tsx
+++ src/components/Codecell.tsx
@@ -1,18 +1,43 @@
+import { Flex, Box, Text } from "@chakra-ui/layout";
+import { Skeleton } from "@chakra-ui/react";
 import React, { useState } from "react";
 import CodeEditor from "./CodeEditor";
 
 function Codecell(props: CodeCellProps) {
-  const { language = "go", initialValue = "", placeholder, onRunCode } = props;
+  const {
+    language = "go",
+    initialValue = "",
+    placeholder,
+    onRunCode,
+    isLoading,
+    number,
+  } = props;
   const [code, setCode] = useState(initialValue);
 
+  const displayedNumber = number ?? " ";
+  const displaySkeleton = !isLoading;
+
   return (
-    <CodeEditor
-      value={code}
-      placeholder={placeholder}
-      onValueChange={(value) => setCode(value)}
-      onCtrlEnter={() => onRunCode(code)}
-      language={language}
-    />
+    <Flex>
+      <Box marginRight="4" marginTop="1">
+        <Skeleton isLoaded={displaySkeleton}>
+          <Text fontFamily="monospace" color="blue.600">
+            In [{displayedNumber}]:{" "}
+          </Text>
+        </Skeleton>
+      </Box>
+      <Box flex="1">
+        <Skeleton isLoaded={displaySkeleton}>
+          <CodeEditor
+            value={code}
+            placeholder={placeholder}
+            onValueChange={(value) => setCode(value)}
+            onCtrlEnter={() => onRunCode(code)}
+            language={language}
+          />
+        </Skeleton>
+      </Box>
+    </Flex>
   );
 }
 
@@ -20,6 +45,8 @@ export interface CodeCellProps {
   // eslint-disable-next-line no-unused-vars
   onRunCode: (value: string) => void;
   language: string;
+  isLoading?: boolean;
+  number?: number;
   initialValue?: string;
   placeholder?: string;
 }
