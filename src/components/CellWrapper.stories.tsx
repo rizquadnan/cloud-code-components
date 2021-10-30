fca82d7abc8acec5a6bb85aec442442cd0cf35ce
adnan <rizquadnan@gmail.com>
2021-10-30 19:05:35 +0700

    implemented cell wrapper

---
+++ src/components/CellWrapper.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import CellWrapper from "./CellWrapper";
+import Codecell from "./Codecell";
+import MarkdownCell from "./MarkdownCell";
+
+export default {
+  title: "CellWrapper",
+  component: CellWrapper,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof CellWrapper>;
+
+const Template: ComponentStory<typeof CellWrapper> = (args) => (
+  <CellWrapper {...args} />
+);
+
+export const WrappingCodeCell = Template.bind({});
+WrappingCodeCell.args = {
+  children: (
+    <Codecell
+      language="go"
+      onRunCode={(value) => console.log("CODE CELL RUN CODE: ", value)}
+      placeholder="This is code cell. Type here. Ctrl + Enter to run code"
+    />
+  ),
+};
+
+export const WrappingCodeCellActive = Template.bind({});
+WrappingCodeCellActive.args = {
+  ...WrappingCodeCell.args,
+  mode: "active",
+};
+
+export const WrappingMarkdownCell = Template.bind({});
+WrappingMarkdownCell.args = {
+  children: <MarkdownCell />,
+};
+
+export const WrappingMarkdownCellActive = Template.bind({});
+WrappingMarkdownCellActive.args = {
+  ...WrappingMarkdownCell.args,
+  mode: "active",
+};
