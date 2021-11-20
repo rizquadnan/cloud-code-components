2e71c0fa922a6550ffa8ab32d29451056ccf2fdc
adnan <rizquadnan@gmail.com>
2021-11-20 12:32:46 +0700

    add onClick props on icon button

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,16 +1,15 @@
 import { Button, ButtonProps } from "@chakra-ui/button";
 import React, { ReactNode } from "react";
 
-function IconButton({ icon, size = "sm" }: ButtonIconProps) {
+function IconButton({ icon, size = "sm", onClick }: ButtonIconProps) {
   return (
-    <Button variant="outline" size={size}>
+    <Button variant="outline" size={size} onClick={onClick}>
       {icon}
     </Button>
   );
 }
 
-export interface ButtonIconProps {
+export interface ButtonIconProps extends Pick<ButtonProps, "size" | "onClick"> {
   icon: ReactNode;
-  size?: ButtonProps["size"];
 }
 export default IconButton;
